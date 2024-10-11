84e3a362a8c2f7400b551590a949323322791a06
Your-Analyst <jnique2finley@gmail.com>
2024-10-11 09:59:48 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const PORT = 3001;
+
+// Read SSL certificate and key
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'server.cert')),
+};
+
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Create HTTPS server
+const server = https.createServer(options, app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
