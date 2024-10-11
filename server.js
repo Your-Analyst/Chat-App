b232691525238f356b1bf1989302dcd12c3094e3
Your-Analyst <jnique2finley@gmail.com>
2024-10-11 14:27:58 -0500

    Update Improved chat UI and fixed timestamp issue

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
-});
 
 //Listen on Provided PORT
 server.listen(PORT, '0.0.0.0', () => {
